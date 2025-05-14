4aa6906eeeb95d1156b2d36655044d993d909ffa
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-14 16:23:22 -0300

    feat: removed files added in hurry and updated exercicio1.cpp

--- cpp/exercicio1.cpp
+++ cpp/exercicio1.cpp
@@ -2,33 +2,87 @@
 #include <string>
 #include <string_view>
 #include <cstdint>
-#include <array>
 
-// wanted to try this way, teacher
-// i could have tried to use a switch 
-// with a bunch of cases in a switch here,
-// it would have been more simple
+void showTotalMatches(int total, int vowels, int consonants, int numbers, std::string_view word) {
+	std::cout << "In the string \"" << word << "\" there is a total of ";
+	std::cout << total << " words, " << vowels << " vowels, " << consonants << " consonants and ";
+	std::cout << numbers << " numbers.\n";
+}
 
-int searchInString(std::string_view stringToSearch) {
-	constexpr std::array<char, 63> whatToSearch { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'b', 'B', 'c', 'C', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'l', 'L', 'm', 'M', 'n', 'N', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ' };
-
-	int matches = 0;
+void searchInString(std::string_view stringToSearch) {
+	int total = 0;
+	int vowel = 0;
+	int consonant = 0;
+	int numbers = 0;
 
 	for(std::size_t i = 0U; i < stringToSearch.length(); i++) {
-		for(std::size_t j = 0U; j < whatToSearch.size(); j++) {
-			if(stringToSearch[i] == whatToSearch[j]) {
-				matches += 1;
-			}
+		switch(stringToSearch[i]) {
+			case 'a':
+				[[fallthrough]];
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+				total+= 1;
+				vowel+= 1;
+			break;
+			case 'b':
+				[[fallthrough]];
+			case 'c':
+			case 'd':
+			case 'f':
+			case 'g':
+			case 'h':
+			case 'j':
+			case 'k':
+			case 'l':
+			case 'm':
+			case 'n':
+			case 'p':
+			case 'q':
+			case 'r':
+			case 's':
+			case 't':
+			case 'v':
+			case 'w':
+			case 'x':
+			case 'y':
+			case 'z':
+				total += 1;
+				consonant += 1;
+			break;
+			case '0':
+				[[fallthrough]];
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				total += 1;
+				numbers += 1;
+			break;
+			default:
+			break;
 		}
+
 	}
 
-	return matches;
+	showTotalMatches(total, vowel, consonant, numbers, stringToSearch);
 }
 
 int main() {
-	std::string word = "Assembly";
+	std::string word = "Assembly12";
 
-	std::cout << searchInString(word) << '\n';
+	searchInString(word);
 
 	return 0;
 }
